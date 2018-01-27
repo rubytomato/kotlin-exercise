9de2614f6ff99aa29a30fdae2b80533830b95a0c
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2018-01-27 15:43:52 +0900

    refactor(test): I added some sample code

--- src/main/java/com/example/exercise/FunctionExercise.java
+++ src/main/java/com/example/exercise/FunctionExercise.java
@@ -1,5 +1,7 @@
 package com.example.exercise;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,6 +14,7 @@ public class FunctionExercise {
 
     public void tryAll() {
         demoFunc();
+        demoFunc2();
     }
 
     private Function<Integer, String> func1 = i -> {
@@ -28,6 +31,46 @@ public class FunctionExercise {
         cons1.accept(22);
         System.out.println(pred1.test(0));
         System.out.println(pred1.test(-1));
+    }
+
+
+    private String func2(Integer i, Function<Integer, String> mapper) {
+        return mapper.apply(i).toString();
+    }
+
+    private void cons2(Integer i, Consumer<Integer> action) {
+        action.accept(i);
+    }
+
+    private Boolean pred2(Integer i, Predicate<Integer> predicate) {
+        return predicate.test(i);
+    }
+
+    private void demoFunc2() {
+        List<String> list = Arrays.asList("A", "B" , "C");
+        list.forEach(System.out::println);
+
+        func2(33, new Function<Integer, String>() {
+            @Override
+            public String apply(Integer i) {
+                Integer temp = i * i;
+                return temp.toString();
+            }
+        });
+
+        cons2(44, new Consumer<Integer>() {
+            @Override
+            public void accept(Integer i) {
+                System.out.println(i * i);
+            }
+        });
+
+        pred2(55, new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer i) {
+                return i >= 0;
+            }
+        });
     }
 
 }
