130126e4f158718d77766f68517fa63cda99b49f
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2018-02-01 15:47:50 +0900

    test(sample): implements test code for assertj

--- src/test/kotlin/com/example/exercise/testing/TestFileUtils.kt
+++ src/test/kotlin/com/example/exercise/testing/TestFileUtils.kt
@@ -6,8 +6,10 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 object TestFileUtils {
+    val LINE_SEPARATOR = System.lineSeparator()!!
+
     fun getResourceFile(fileName: String, dir: String = "data/template") : Path =
-            Paths.get(this.javaClass.classLoader.getResource("$dir/$fileName").toURI())
+        Paths.get(this.javaClass.classLoader.getResource("$dir/$fileName").toURI())
 
     fun createTestTempFile(contents: List<String>, prefix: String = "test_", suffix: String = ".txt") : File =
         Files.createTempFile(prefix, suffix).toFile().apply {
