c2a961fd6cb8628db5ab87a01e3f505c879c4367
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2018-02-05 01:15:08 +0900

    feat(sample): Added function sample code

--- src/main/java/com/example/exercise/FunctionExercise.java
+++ src/main/java/com/example/exercise/FunctionExercise.java
@@ -73,4 +73,8 @@ public class FunctionExercise {
         });
     }
 
+    public static Predicate<String> checkIfStartsWith(final String letter) {
+        return name -> name.startsWith(letter);
+    }
+
 }
